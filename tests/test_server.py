ccc8a609b4a6ed7f807e639a085ccd93a1002655
Igor <pierre.zarebski@live.fr>
2026-01-19 20:07:43 +0100

    docs

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,13 +6,16 @@ import pytest
 
 @pytest.fixture
 def client(monkeypatch):
+    # Client de test pour requêtes
     return app.test_client()
 
 def test_showSummary_sad_invalid(client):    
+    # Tentative de connexion par une email invalide
     response = client.post('/showSummary', data=dict(email='inconnu@test.com'))
     assert(response.status_code == 500)
     assert b'Email invalide' in response.data
 
 def test_showSummary_happy_valid(client):
+    # Tentative de connexion par une email valide
     response = client.post('/showSummary', data=dict(email='admin@irontemple.com'))
     assert(response.status_code == 200)
