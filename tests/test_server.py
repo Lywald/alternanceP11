217fc3aea230c24a4599b3c4fb7901a21722ebf2
Igor <pierre.zarebski@live.fr>
2026-01-19 18:54:29 +0100

    clean test fi le

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,47 +4,6 @@ from server import loadClubs
 from server import purchasePlaces
 import pytest
 
-"""
-@pytest.fixture
-def competitions_fixture():
-    return [ {
-        "name": "Pierre Festival",
-        "date": "2020-03-27 10:00:00",
-        "numberOfPlaces": "25"
-    } ]
-
-@pytest.fixture
-def clubs_fixture():
-    return [ {
-        "name": "Pierre Club",
-        "email": "pierre@pierre.com",
-        "points": "10"
-    } ]
-
-def test_purchasePlaces(competitions_fixture, clubs_fixture):
-    # Test purchasing places
-    print("test_purchasePlaces")
-    
-    assert int(competitions_fixture[0]['numberOfPlaces']) == 25
-    assert int(clubs_fixture[0]['points']) == 10
-
-    # app.competitions = competitions_fixture
-    # app.clubs = clubs_fixture
-    with app.test_client() as client:
-        response = client.post('/purchasePlaces', data=dict(places=1, competition="Pierre Festival", club="Pierre Club"))
-        assert response.status_code == 200
-        assert b'Great-booking complete!' in response.data
-        assert int(competitions_fixture[0]['numberOfPlaces']) == 24
-        assert int(clubs_fixture[0]['points']) == 9
-
-        print(competitions_fixture[0]['numberOfPlaces'])
-        print(clubs_fixture[0]['points'])
-
-        assert int(competitions_fixture[0]['numberOfPlaces']) == 24
-        assert int(clubs_fixture[0]['points']) == 9
-
-"""
-
 @pytest.fixture
 def competitions_fixture():
     return [ {
@@ -66,17 +25,6 @@ def client(competitions_fixture, clubs_fixture, monkeypatch):
     monkeypatch.setattr('server.clubs', clubs_fixture)
     monkeypatch.setattr('server.competitions', competitions_fixture)
     return app.test_client()
-
-"""
-def test_showSummary_sad_invalid(client):    
-    with pytest.raises(IndexError):
-        response = client.post('/showSummary', data=dict(email='inconnu@test.com'))
-        # assert(response.status_code == 500)
-
-def test_showSummary_happy_valid(client):
-    response = client.post('/showSummary', data=dict(email='admin@irontemple.com'))
-    assert(response.status_code == 200)
-"""
 
 def test_purchasePlaces_sad_invalid_competition(competitions_fixture, clubs_fixture):
     with app.test_client() as client:
