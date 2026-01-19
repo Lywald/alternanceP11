45c09a2e2ea3f3807093a0166a27163789dbbe0f
Igor <pierre.zarebski@live.fr>
2026-01-19 19:03:26 +0100

    better test_showSummary_sad_invalid

--- tests/test_server.py
+++ tests/test_server.py
@@ -9,9 +9,8 @@ def client(monkeypatch):
     return app.test_client()
 
 def test_showSummary_sad_invalid(client):    
-    with pytest.raises(IndexError):
-        response = client.post('/showSummary', data=dict(email='inconnu@test.com'))
-        # assert(response.status_code == 500)
+    response = client.post('/showSummary', data=dict(email='inconnu@test.com'))
+    assert(response.status_code == 500)
 
 def test_showSummary_happy_valid(client):
     response = client.post('/showSummary', data=dict(email='admin@irontemple.com'))
