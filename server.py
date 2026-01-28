74bebac9fec7c17080b5e7f17b8c5c566701b5a4
Igor <pierre.zarebski@live.fr>
2026-01-28 16:43:03 +0100

    server fixed showSummary invalid email

--- server.py
+++ server.py
@@ -26,8 +26,10 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club_list = [club for club in clubs if club['email'] == request.form['email']]
+    if not club_list:
+        return render_template('error.html'), 500
+    return render_template('welcome.html', club=club_list[0], competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
